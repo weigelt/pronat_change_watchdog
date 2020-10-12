b57b9bb6b401475c63243e99749136cf3f91de5d
Sebastian Weigelt <weigelt@kit.edu>
2020-10-12 19:35:57 +0200

    basic watchdog implementation

--- src/main/java/edu/kit/ipd/parse/changeWD/ChangeWatchdog.java
+++ src/main/java/edu/kit/ipd/parse/changeWD/ChangeWatchdog.java
@@ -5,8 +5,6 @@ import edu.kit.ipd.parse.luna.agent.AbstractAgent;
 import edu.kit.ipd.parse.luna.agent.AbstractWatchdog;
 import edu.kit.ipd.parse.luna.tools.ConfigManager;
 import org.kohsuke.MetaInfServices;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
@@ -19,13 +17,16 @@ import java.util.Properties;
 public class ChangeWatchdog extends AbstractWatchdog {
 
 	private static final String ID = "changeWatchdog";
-	private static final Logger logger = LoggerFactory.getLogger(ChangeWatchdog.class);
 	private static final Properties wdProps = ConfigManager.getConfiguration(ChangeWatchdog.class);
 	private static final Properties lunaProps = ConfigManager.getConfiguration(AbstractLuna.class);
 
+	private static final String PROP_TIMEOUT_THRESHOLD = "CHANGE_TIMEOUT_THRESHOLD";
 	private static final String PROP_TERM_SIGNAL_TYPE = "TERM_SIGNAL_TYPE";
 
 	private String termSignalType;
+	private long cto_threshold;
+	private long lastChangeTime = -1;
+	private long lastGraphHash = -1;
 
 	public ChangeWatchdog() {
 		setId(ID);
@@ -36,6 +37,7 @@ public class ChangeWatchdog extends AbstractWatchdog {
 	 */
 	@Override
 	public void init() {
+		cto_threshold = Long.parseLong(wdProps.getProperty(PROP_TIMEOUT_THRESHOLD));
 		termSignalType = lunaProps.getProperty(PROP_TERM_SIGNAL_TYPE);
 	}
 
@@ -45,5 +47,15 @@ public class ChangeWatchdog extends AbstractWatchdog {
 	@Override
 	protected void exec() {
 
+		long currHash = graph.hashCode();
+		if (lastGraphHash != currHash) {
+			lastChangeTime = System.currentTimeMillis();
+		} else {
+			long currTime = System.currentTimeMillis();
+			if (currTime - lastChangeTime > cto_threshold) {
+				graph.createNodeType(termSignalType);
+			}
+		}
+		lastGraphHash = currHash;
 	}
 }
--- src/test/java/edu/kit/ipd/parse/changeWD/ChangeWatchdogTest.java
+++ src/test/java/edu/kit/ipd/parse/changeWD/ChangeWatchdogTest.java
@@ -36,4 +36,74 @@ public class ChangeWatchdogTest {
 		cwd = new ChangeWatchdog();
 		cwd.init();
 	}
+
+	/**
+	 * Tests whether the agent has added the term node type if the graph remains
+	 * unaltered after the timeout exceeded.
+	 */
+	@Test
+	public void testExec4000msNoChange() {
+
+		wdProps.setProperty("TIMEOUT_THRESHOLD", "4000");
+		cwd.setGraph(graph);
+		cwd.exec();
+		try {
+			Thread.sleep(4100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		cwd.setGraph(graph);
+		cwd.exec();
+		assertTrue(graph.hasNodeType("terminate"));
+	}
+
+	/**
+	 * Tests whether the agent doesn't add the term node before the timeout is
+	 * exceeded and the graph remains unaltered.
+	 */
+	@Test
+	public void testExec5000msNoChangeFail() {
+
+		wdProps.setProperty("TIMEOUT_THRESHOLD", "5000");
+		cwd.setGraph(graph);
+		cwd.exec();
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		cwd.setGraph(graph);
+		cwd.exec();
+		assertFalse(graph.hasNodeType("terminate"));
+	}
+
+	/**
+	 * Tests whether the agent doesn't add the term node after 7 consecutive graph
+	 * changes (with a timeout of 1000ms) and has added the term node type after
+	 * another timeout of 6000ms without a change to the graph.
+	 */
+	@Test
+	public void testMultExec5000ms() {
+
+		wdProps.setProperty("TIMEOUT_THRESHOLD", "5000");
+		for (int i = 0; i < 7; i++) {
+			graph.createNodeType(String.valueOf(i));
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			cwd.setGraph(graph);
+			cwd.exec();
+			assertFalse(graph.hasNodeType("terminate"));
+		}
+		try {
+			Thread.sleep(6000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		cwd.setGraph(graph);
+		cwd.exec();
+		assertTrue(graph.hasNodeType("terminate"));
+	}
 }
